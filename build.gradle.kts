9f77d25ad350685aabc3218833ae17786ef99b89
Damian Rafael Lattenero <damianlattenero@gmail.com>
2026-03-20 18:16:46 -0300

    deps: bump dokka from 1.9.20 to 2.1.0
    Closes #4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform") version "2.0.21" apply false
     kotlin("jvm") version "2.0.21" apply false
-    id("org.jetbrains.dokka") version "1.9.20" apply false
+    id("org.jetbrains.dokka") version "2.1.0" apply false
     id("com.vanniktech.maven.publish") version "0.30.0" apply false
 }
 
